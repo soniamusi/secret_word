35dc0ba01e3a55c0e985be957f1f3e3b5f03759c
Sonia Muniz <soninhamusi@gmail.com>
2024-09-18 10:15:15 -0400

    projeto-secret-word

---
+++ src/data/word.js
@@ -0,0 +1,8 @@
+export const wordList = {
+  carro: ["Motor", "Porta", "Capô", "Peneu", "Antena"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  corpo: ["Braço", "Perna", "Cerebro", "Pescoço", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programacao: ["Linguagem", "Framwork", "Javascript", "React"],
+  Alimento: ["Arroz", "Feijão", "Carne", "Leite","Ovo"],
+};
